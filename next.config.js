1a8d30e49e81a7181cb24dcb0f5e1ec22324982a
Abdul Mohaimin | Mobinius <abdul.mn@mobinius.com>
2021-03-15 11:14:50 +0530

    updated next js

--- next.config.js
+++ next.config.js
@@ -1,27 +1,7 @@
-const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
-
-const withNextOptimizedImages = require('next-optimized-images');
-
-const withMDX = require('@next/mdx')({
-    extension: /\.mdx?$/,
-})
-
-module.exports = withNextOptimizedImages(
-    withMDX({
-        webpack: (config, { isServer }) => {
-            if (!isServer) {
-                config.node = {
-                    fs: 'empty'
-                }
-            }
-            return config
-        },
-        pageExtensions: ['js', 'jsx', 'mdx'],
-        target: 'serverless',
-        basePath: ghPages ? '/blog' : '',
-        assetPrefix: ghPages ? '/blog' : '',
-        images: {
-            loader: "imgix",
-        }
-    })
-)
+module.exports = {
+    assetPrefix: '',
+    basePath: "/blog",
+    images: {
+        loader: "imgix",
+    }
+}
